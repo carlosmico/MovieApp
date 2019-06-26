2f2e3520920c86bf25992223c08588287dada8f4
Carlos Micó <carlosmico15@gmail.com>
2019-06-26 17:36:19 +0200

    Corrección ruta movie

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,7 +7,7 @@ import {MovieComponent} from './movie/movie.component'
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
-  {path: '/movie', component: MovieComponent},
+  {path: 'movie', component: MovieComponent},
 ];
 
 @NgModule({
