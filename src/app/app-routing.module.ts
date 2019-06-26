3b6719a78c5f17d26a788d49cd7a81f52318fe98
Carlos Micó <carlosmico15@gmail.com>
2019-06-26 17:27:30 +0200

    Router configuration

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+//Componentes
+import {HomeComponent} from './home/home.component'
+import {MovieComponent} from './movie/movie.component'
+
+const routes: Routes = [
+  {path: '/', component: HomeComponent},
+  {path: '/movie', component: MovieComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
