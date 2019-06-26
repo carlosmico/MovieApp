cc5788afb129bf6e9f0fa42fa234b069c49e1f8e
GuiBoCo <guiboco18@gmail.com>
2019-06-26 16:20:20 +0200

    created incoming component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PeliculasOnlineComponent } from './peliculas-online/peliculas-online.component';
+import { IncomingComponent } from './incoming/incoming.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PeliculasOnlineComponent
+    PeliculasOnlineComponent,
+    IncomingComponent
   ],
   imports: [
     BrowserModule,
