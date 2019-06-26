dd8eed2e77e2e61db1b72d7c2aa6c0d24a424011
Iris <irisromeropiquer@gmail.com>
2019-06-26 16:14:20 +0200

    pruebas

---
+++ src/app/peliculas-online/peliculas-online.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-peliculas-online',
+  templateUrl: './peliculas-online.component.html',
+  styleUrls: ['./peliculas-online.component.scss']
+})
+export class PeliculasOnlineComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
