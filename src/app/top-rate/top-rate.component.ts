9fbcf52331e7cd112b5f33d1aa57bbaee709a3ee
GuiBoCo <guiboco18@gmail.com>
2019-06-30 12:01:31 +0200

    merge conflict top-rate.components.ts resolve

--- src/app/top-rate/top-rate.component.ts
+++ src/app/top-rate/top-rate.component.ts
@@ -13,15 +13,9 @@ export class TopRateComponent implements OnInit {
 
    
   ngOnInit() {
-<<<<<<< HEAD
     this.MoviesService.getTopRatedMovies().subscribe(value => {
       this.movies = value.results;
     }, err=> console.log(err));
-=======
-    // this.MoviesService.getTopRateMovies().subscribe(value => {
-    //   this.movies = value.results;
-    // }, err=> console.log(err));
->>>>>>> origin/master
   }
 
 }
