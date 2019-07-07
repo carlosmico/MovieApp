e82d5884c04668c164631ddeb077d8b57639c0c1
Iris <irisromeropiquer@gmail.com>
2019-07-07 16:56:11 +0200

    merge conflict requetesolved

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,16 +8,11 @@ import { PopularMoviesComponent } from './movies/popular-movies/popular-movies.c
 import { MovieComponent } from './movies/movie/movie.component'
 import { TopRateComponent } from './movies/top-rate/top-rate.component'
 import { UpcomingMoviesComponent } from './movies/upcoming-movies/upcoming-movies.component';
-<<<<<<< HEAD
-import { RandomComponent } from './movies/random/random.component'
-import { Notfound404Component} from './notfound404/notfound404.component';
 import { PopularPeopleComponent } from './people/popular-people/popular-people.component';
-=======
 import {RandomComponent} from './movies/random/random.component';
 import {ListsComponent} from './movies/lists/lists.component';
 import { Notfound404Component} from './notfound404/notfound404.component';
 import {ListDetailsComponent} from './movies/lists/list-details/list-details.component'
->>>>>>> 9785d6cce28e1bfbde774dbc7281de76a9d9b608
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,14 +16,11 @@ import { MainComponent } from './main/main.component';
 import { RandomComponent } from './movies/random/random.component';
 import { DiscoverMoviesComponent } from './movies/discover-movies/discover-movies.component';
 import { Notfound404Component } from './notfound404/notfound404.component';
-<<<<<<< HEAD
 import { PopularPeopleComponent } from './people/popular-people/popular-people.component';
-=======
 import { ListsComponent } from './movies/lists/lists.component';
 import { BuyTicketsComponent } from './buy-tickets/buy-tickets.component';
 import { SearchBarComponent } from './search-bar/search-bar.component';
 import { ListDetailsComponent } from './movies/lists/list-details/list-details.component';
->>>>>>> 9785d6cce28e1bfbde774dbc7281de76a9d9b608
 
 @NgModule({
   declarations: [
@@ -40,14 +37,11 @@ import { ListDetailsComponent } from './movies/lists/list-details/list-details.c
     RandomComponent,
     DiscoverMoviesComponent,
     Notfound404Component,
-<<<<<<< HEAD
-    PopularPeopleComponent
-=======
+    PopularPeopleComponent,
     ListsComponent,
     BuyTicketsComponent,
     SearchBarComponent,
     ListDetailsComponent
->>>>>>> 9785d6cce28e1bfbde774dbc7281de76a9d9b608
   ],
   imports: [
     BrowserModule,
