11a221442d6dc6136c87c5656f03a9e8d9a29418
GuiBoCo <guiboco18@gmail.com>
2019-07-10 17:59:13 +0200

    updated css register and login

--- frontend/src/app/users/login/login.component.ts
+++ frontend/src/app/users/login/login.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import {FormGroup, FormControl, Validators} from '@angular/forms';
+import {UsersService,} from '../users.service';
+import {ActivatedRoute} from '@angular/router'
+
+
+
 
 @Component({
   selector: 'app-login',
@@ -6,10 +12,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  form:FormGroup;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, private usersServices: UsersService) { }
 
   ngOnInit() {
-  }
+  this.form = new FormGroup({
+    username: new FormControl("", { validators: [Validators.minLength(8)] }),
+    email: new FormControl("", { validators: [Validators.email, Validators.required] }),
+    password: new FormControl("", { validators: [Validators.pattern(/^(?=.*[0-9]+.*)(?=.*[a-zA-Z]+.*)[0-9a-zA-Z]{8,}$/), Validators.required] })
+  })
 
+  }
+  handleSubmit() {
+    console.log(this.form)
+    if (this.form.status === "VALID") {
+      console.log(this.form)
+      //register es el método definido en el serivicio
+      //this.form.value tiene los datos que hemos metido en el formulario
+      this.usersServices.register(this.form.value).subscribe(res => {
+        localStorage.setItem('authToken', res.token)
+        this.usersServices.isAuth = true;
+        console.log(res)
+      },
+        error => console.log(error)) //SUBSCRIBE ES DE OBSVERVABLES. MIRAR OBSERVABLES
+    }
+  }
 }
+
+
